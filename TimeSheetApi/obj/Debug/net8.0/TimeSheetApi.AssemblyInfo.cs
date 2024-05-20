41fb94ec65905fdbd77971386b66de2c8e87480d
JaswanthChigurupati <jaswanthchigurupati@gmail.com>
2024-05-20 13:10:46 +0100

    removed the deploy files

--- TimeSheetApi/obj/Debug/net8.0/TimeSheetApi.AssemblyInfo.cs
+++ TimeSheetApi/obj/Debug/net8.0/TimeSheetApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeSheetApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8408e1b48e4d204a54ad035718bf7bcc83cf5f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimeSheetApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimeSheetApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
