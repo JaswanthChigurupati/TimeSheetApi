f0469287eed6f4cd44cb62894a37ad2e0d235fa8
JaswanthChigurupati <jaswanthchigurupati@gmail.com>
2024-05-20 14:36:28 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,59 @@
+variable "backend_resource_group_name" {
+  description = "The name of the resource group for the Terraform backend"
+  type        = string
+}
+
+variable "backend_storage_account_name" {
+  description = "The name of the storage account for the Terraform backend"
+  type        = string
+}
+
+variable "backend_container_name" {
+  description = "The name of the container for the Terraform backend"
+  type        = string
+}
+
+variable "backend_key" {
+  description = "The key for the Terraform state file"
+  type        = string
+}
 
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "aks-resource-group"
 }
 
 variable "location" {
   description = "The Azure location where the resources will be created"
   type        = string
-  default     = "East US"
 }
 
 variable "aks_cluster_name" {
   description = "The name of the AKS cluster"
   type        = string
-  default     = "aks-cluster"
 }
 
 variable "dns_prefix" {
   description = "The DNS prefix for the AKS cluster"
   type        = string
-  default     = "akscluster"
 }
 
 variable "node_count" {
   description = "The number of nodes in the AKS cluster"
   type        = number
-  default     = 2
 }
 
 variable "vm_size" {
   description = "The size of the virtual machines in the AKS cluster"
   type        = string
-  default     = "Standard_DS2_v2"
+}
+
+variable "network_plugin" {
+  description = "The network plugin for the AKS cluster"
+  type        = string
+}
+
+variable "load_balancer_sku" {
+  description = "The load balancer SKU for the AKS cluster"
+  type        = string
 }
